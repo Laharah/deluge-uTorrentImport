3030fab98afcab35423301eaa5b3e77f0d26b2f4
Laharah <laharah22@gmail.com>
2015-08-26 12:14:48 -0700

    - added the AsyncDialog class     a non-blocking dialog usefull for displaying simple messages

---
+++ utorrentimport/dialogs.py
@@ -0,0 +1,34 @@
+
+import gtk
+
+
+class AsyncDialog(gtk.Dialog):
+    """
+    A gtk Dialog that does not block.
+    has these additional arguments:
+    response_callback: the callback to handle the response signal
+    destroy_signals: A convince for setting signals that will destroy the dialog.
+    """
+
+    def __init__(self, title=None, parent=None, flags=None, buttons=None,
+                 response_callback=None, destroy_signals=None):
+        gtk.Dialog.__init__(self, title, parent, flags, buttons)
+
+        self.response_callback = response_callback
+
+        if not isinstance(destroy_signals, list):
+            destroy_signals = [destroy_signals]
+        self.destroy_signals = destroy_signals
+
+
+    def run(self):
+        """a version of gtk.Dialog.run that does not block"""
+        def dialog_response_cb(dialog, response_id):
+            if response_id in self.destroy_signals:
+                self.destroy()
+            self.response_callback(response_id)
+
+        self.connect('response', dialog_response_cb)
+        if not self.modal:
+            self.set_modal(True)
+        self.show()
